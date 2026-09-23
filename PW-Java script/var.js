449e1b6d2cdc776ba30571f9312efe845cecc313
Bernardo Schnell <be.sschnell@gmail.com>
2026-09-23 20:36:10 -0300

    Add files via upload

---
+++ PW-Java script/var.js
@@ -0,0 +1,9 @@
+function algo (id1, id2){
+    let inp1 = document.getElementById(id1).value;
+    let inp2 = document.getElementById(id2).value;
+
+    console.log(inp1)
+
+    console.log(inp2)
+
+}
